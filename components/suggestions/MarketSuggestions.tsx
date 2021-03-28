4f5a1a7a1a36d363be6debf463ece1b6bdccb0a3
Claudiu C. Bogdan <claudiu.engineer@gmail.com>
2021-03-28 13:47:20 +0300

    feat: add responsive layout to home cards

--- components/suggestions/MarketSuggestions.tsx
+++ components/suggestions/MarketSuggestions.tsx
@@ -5,13 +5,18 @@ import {Swiper, SwiperSlide} from "swiper/react";
 import DetailedMarketSummary from "./DetailedMarketSummary";
 import MarketSummary from "./MarketSummary";
 import styles from './styles/Suggestions.module.scss'
+import {breakpoints, useWindowSize} from "../../utils/window";
 
 export default function MarketSuggestions() {
 
+    const windowSize = useWindowSize()
     const {data: results, error} = useSWR<MarketSuggestionData>(`/api/suggestions/markets/`, fetcher)
+
     const sections = results ? results.data.sections : []
     const topSections = (results && results.data.topSections) ?? [];
 
+    const detailedCardsPerView = getDetailedCardsPerView(windowSize.width)
+    const secondaryCardsPerView = getSecondaryCardsPerView(windowSize.width)
 
     return (<div>
         <div>
@@ -23,7 +28,7 @@ export default function MarketSuggestions() {
                     <Swiper
                         className={styles['container']}
                         //FIXME: Change value base on screen width
-                        slidesPerView={4}
+                        slidesPerView={detailedCardsPerView}
                         centeredSlides={true}
                         loop={true}
                         autoplay={{delay: 3000, disableOnInteraction: false}}
@@ -47,7 +52,7 @@ export default function MarketSuggestions() {
                     </h2>
                     <Swiper
                         className={styles['container']}
-                        slidesPerView={8}
+                        slidesPerView={secondaryCardsPerView}
                         spaceBetween={25}
                         centeredSlides={true}
                         loop={true}>
@@ -64,4 +69,59 @@ export default function MarketSuggestions() {
         </div>
 
     </div>)
+}
+
+function getDetailedCardsPerView(size: number) {
+
+    const defaultViews = 1
+
+    if (size > breakpoints.screenXXl) {
+        return 4
+    }
+
+    if (size > breakpoints.screenXl) {
+        return 3.5
+    }
+
+    if (size > breakpoints.screenLd) {
+        return 2.5
+    }
+
+    if (size > breakpoints.screenMd) {
+        return 2
+    }
+
+    if (size > breakpoints.screenSm) {
+        return 1.5
+    }
+
+    return defaultViews
+}
+
+
+function getSecondaryCardsPerView(size: number) {
+
+    const defaultViews = 2
+
+    if (size > breakpoints.screenXXl) {
+        return 8
+    }
+
+    if (size > breakpoints.screenXl) {
+        return 6.5
+    }
+
+    if (size > breakpoints.screenLd) {
+        return 4.5
+    }
+
+    if (size > breakpoints.screenMd) {
+        return 4
+    }
+
+    if (size > breakpoints.screenSm) {
+        return 3
+    }
+
+    return defaultViews
 }
